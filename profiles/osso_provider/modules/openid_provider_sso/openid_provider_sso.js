38f83a855271fa731c419dc10a4ffa376149be0f
Alex Barth <ab@a.local>
2009-11-24 10:54:05 -0500

    Simplify / fix path replacement.

--- profiles/osso_provider/modules/openid_provider_sso/openid_provider_sso.js
+++ profiles/osso_provider/modules/openid_provider_sso/openid_provider_sso.js
@@ -8,23 +8,21 @@ Drupal.behaviors.openid_provider_sso = function() {
   // Retrieve realm
   // Example: user&realm=http%3A%2F%2Flocalhost%2Frp%
   var vars = window.location.search.replace('?', '').split("&");
-
+  var realm = '';
   for (i = 0; i < vars.length; i++) {
-
     var value = vars[i].split("=");
-
     if (value[0] == 'realm') {
       realm = vars[i];
       break;
     }
   }
-      console.log(realm);
+
   // Attach realm to all user and user/register links.
   if (realm != '') {
-    var basePath = Drupal.settings.basePath;
-    $("a[href='" + basePath + "?q=user/register']").attr('href', basePath + '?q=user/register&' + realm);
-    $("a[href='" + basePath + "user/register']").attr('href', basePath + 'user/register?' + realm);
-    $("a[href='" + basePath + "?q=user']").attr('href', basePath + '?q=user&' + realm);
-    $("a[href='" + basePath + "user']").attr('href', basePath + 'user?' + realm);
+    var base = Drupal.settings.basePath;
+    $("a[href='" + base + "?q=user/register']").attr('href', base + '?q=user/register&' + realm);
+    $("a[href='" + base + "user/register']").attr('href', base + 'user/register?' + realm);
+    $("a[href='" + base + "?q=user']").attr('href', base + '?q=user&' + realm);
+    $("a[href='" + base + "user']").attr('href', base + 'user?' + realm);
   }
 }
