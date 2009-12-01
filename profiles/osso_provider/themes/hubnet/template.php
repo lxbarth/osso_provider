388614d4044bcaa1602637fa06e9c999df2879ed
aj <aj@xps.localdomain>
2009-12-01 17:40:11 -0500

    Theme changes.

--- profiles/osso_provider/themes/hubnet/template.php
+++ profiles/osso_provider/themes/hubnet/template.php
@@ -2,5 +2,17 @@
 
 function hubnet_preprocess_page (&$vars) {
   $vars['site_slogan'] = variable_get('site_slogan', '');
-  //krumo($vars);
+  if ($logged_in == FALSE && $vars['template_files']['0'] == 'page-user') {
+    unset($vars['tabs']);
+  }
+  //krumo($vars); 
 }
+
+function hubnet_preprocess_node (&$vars) {
+  $item = menu_get_object();
+  krumo($vars);
+  if ($item == $vars["node"]) {
+    unset($vars["title"]);
+  }
+}
+
