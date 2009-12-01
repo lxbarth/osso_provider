cda2e5fba5a8641c2602e781bd244ee44fabf1c8
Alex Barth <ab@a.local>
2009-12-01 15:29:27 -0500

    Add activity type classes.

--- profiles/osso_provider/themes/hubnet/template.php
+++ profiles/osso_provider/themes/hubnet/template.php
@@ -4,3 +4,14 @@ function hubnet_preprocess_page (&$vars) {
   $vars['site_slogan'] = variable_get('site_slogan', '');
   //krumo($vars);
 }
+
+/**
+ * Add activity type classes.
+ */
+function hubnet_preprocess_views_view_fields(&$vars) {
+  if ($type = $vars['row']->node_feeds_data_activity_feed_node_data_field_type_field_type_value) {
+    if (isset($vars['fields']['field_type_value'])) {
+      $vars['fields']['field_type_value']->class .= ' activity-type-'. strtolower($type);
+    }
+  }
+}
