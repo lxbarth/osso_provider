e7c7911e309db635b03dfe362870fe72f9525baa
Alex Barth <ab@a.local>
2009-12-22 17:50:19 -0500

    Refactor destination loop-through.

--- profiles/osso_provider/modules/openid_provider_sso/openid_provider_sso.js
+++ profiles/osso_provider/modules/openid_provider_sso/openid_provider_sso.js
@@ -4,27 +4,14 @@
  */
 
 Drupal.behaviors.openid_provider_sso = function() {
-
-  // Retrieve realm
-  // Example: user&realm=http%3A%2F%2Flocalhost%2Frp%
-  var vars = window.location.search.replace('?', '').split("&");
-  var realm = '';
-  for (i = 0; i < vars.length; i++) {
-    var value = vars[i].split("=");
-    if (value[0] == 'realm') {
-      realm = vars[i];
-      break;
-    }
-  }
-
-  // Attach realm to all user and user/register links.
-  if (realm != '') {
+  if (Drupal.settings.destination) {
     var base = Drupal.settings.basePath;
-    $("a[href='" + base + "?q=user/register']").attr('href', base + '?q=user/register&' + realm);
-    $("a[href='" + base + "user/register']").attr('href', base + 'user/register?' + realm);
-    $("a[href='" + base + "?q=user']").attr('href', base + '?q=user&' + realm);
-    $("a[href='" + base + "user']").attr('href', base + 'user?' + realm);
-    $("a[href='" + base + "?q=user/password']").attr('href', base + '?q=user/password&' + realm);
-    $("a[href='" + base + "user/password']").attr('href', base + 'user/password?' + realm);
+    var destination = "destination=" + Drupal.settings.destination;
+    $("a[href='" + base + "?q=user/register']").attr('href', base + '?q=user/register&' + destination);
+    $("a[href='" + base + "user/register']").attr('href', base + 'user/register?' + destination);
+    $("a[href='" + base + "?q=user']").attr('href', base + '?q=user&' + destination);
+    $("a[href='" + base + "user']").attr('href', base + 'user?' + destination);
+    $("a[href='" + base + "?q=user/password']").attr('href', base + '?q=user/password&' + destination);
+    $("a[href='" + base + "user/password']").attr('href', base + 'user/password?' + destination);
   }
 }
